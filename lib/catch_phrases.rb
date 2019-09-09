afb985a3e60b4b2199216875c7da9e1a5a8a5098
epic-constructor-9765 <barton.e.marks@gmail.com>
2019-09-09 23:39:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,9 @@
 $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9d8d336b1572b177a87864accd08f7f47ac84189
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
@@ -14,7 +17,12 @@ def link
   puts  "It's Dangerous To Go Alone! Take This."
 end
 
+<<<<<<< HEAD
 
 def any_phrase(phrase)
   puts "Do A Barrel Roll!"
+=======
+phrase = "Do A Barrel Roll!"
+def any_phrase(phrase)
+>>>>>>> 9d8d336b1572b177a87864accd08f7f47ac84189
 end
